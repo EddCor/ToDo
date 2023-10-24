7b217d16beea96db31b985e2a8bc848d3eeadabc
Edd Coria <jegcoria@gmail.com>
2023-10-24 20:03:19 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import ItemForm from './components/ItemForm';
+import Item from './components/Item';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const storedItems = JSON.parse(localStorage.getItem('items'));
+    if (storedItems) {
+      setItems(storedItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    localStorage.setItem('items', JSON.stringify(items));
+  }, [items]);
+
+  function addItem(name) {
+    setItems((prev) => [...prev, { name: name, done: false }]);
+  }
+
+  function removeItem(indexToErase) {
+    setItems((prev) => prev.filter((_, index) => index !== indexToErase));
+  }
+
+
+  function updateItemDone(itemIndex, newDone) {
+    setItems((prev) => {
+      const newItems = [...prev];
+      newItems[itemIndex].done = newDone;
+      return newItems;
+    });
+  }
+
+
+  const numberComplete = items.filter(i => i.done).length;
+  const numberTotal =items.length
+
+function getMessage(){
+  const percentage = numberComplete/numberTotal*100;
+  if (percentage === 0){
+    return 'Lest Start ✍🏼'
+  }
+  if(percentage === 100){
+    return 'You are a Rockstar 🪩'
+  }
+  return "You're almost  D O N E 🧐 "
+}
+
+
+function renameItem(index, newName) {
+  const itemsCopy = JSON.parse(JSON.stringify(items))
+ itemsCopy[index].name=newName
+ console.log (itemsCopy)
+  setItems(itemsCopy);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>{numberComplete}/{numberTotal} Complete</h1>
+      <h2> {getMessage()}</h2>
+      <ItemForm onAdd={addItem} />
+      {items.map((item, index) => (
+        <Item
+          key={index}
+          {...item}
+          onRename={(newName) => renameItem(index, newName)}
+          onErase={() => removeItem(index)}
+          onToggle={(done) => updateItemDone(index, done)}
+        />
+      ))}
+    </main>
   );
 }
 
---
+++ src/components/ItemForm.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+
+
+export default function ItemForm ({onAdd}) {
+const[itemName, setItemName]= useState('');
+function handleSubmit(ev){
+ev.preventDefault();
+onAdd(itemName);
+setItemName('')
+}
+return(
+    <form onSubmit={handleSubmit}>
+    <button>+</button>
+    <input type="text"
+    value={itemName}
+    onChange={ev => setItemName(ev.target.value)}
+    placeholder= "Next item"
+     />
+  </form>
+
+);
+}
